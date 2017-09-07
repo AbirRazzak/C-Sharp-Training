86d2487c633bf6e10b6738897c7ca309b7986f47
Abir Razzak <amr439@drexel.edu>
2017-09-06 20:41:07 -0400

    Project 2/3 - Windows Form Project & Discord .NET Bot
    Attempted to create a windows form application. Got bored of it...
    
    But I also made a Discord Bot! It was pretty fun! Lots of things learned
    and implemented from my online Sololearn tutorials! Made use of Jagged
    Arrays and other techniques for commands and setting up the bot!

---
+++ Discord Bot/Visual Studio Project/Discord Bot/Discord Bot/Program.cs
@@ -0,0 +1,124 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+
+namespace Discord_Bot
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        public async Task MainAsync()
+        {
+            string token = "MzI4MDM1OTA2NTU0NDI5NDQ0.DJH32w.gP2qdCGhI2coMLIAwxSSDkvMt04";
+
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            
+            _client.MessageReceived += MessageReceived;
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (input != "exit");
+
+            await _client.StopAsync();
+            await _client.LogoutAsync();            
+        }
+
+        private Task _client_LoggedOut()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public String GetCommands(String[][] cmds)
+        {
+            String _return = "This bot has the following commands: \n";
+
+            for (int i = 0; i < cmds.Length; i++)
+            {
+                _return += cmds[i][0];
+                _return += "\n";
+            }
+
+            return _return;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            String msg = message.Content;
+            
+            //Text Commands
+            String[][] textCmds = new String[][]
+            {
+                new String[]{"!help",   ""},
+                new String[]{"!test",   "Bot is working"},
+                new String[]{"!plug",   "https://plug.dj/-9024781283140214337"},
+                new String[]{"!poe",    "@Guano wanna play PoE LUL"},
+                new String[]{"!no",     "Fuck off Abir"},
+                new String[]{"!hello",  "Hi!"}
+            };
+                        
+            //if ( !(msg.Contains(" ")) )
+            //{
+                int i = textCmds.Length - 1;
+                for (; i > -1; i--)
+                {
+                    if (msg == "!help") textCmds[0][1] = GetCommands(textCmds);
+                    if (textCmds[i][0].Contains(msg))
+                    {
+                        await message.Channel.SendMessageAsync(textCmds[i][1]);
+                        String _logMessage = "Issued " + textCmds[i][0] + " command";
+                        LogMessage _log = new LogMessage(LogSeverity.Info, "User", _logMessage);
+                        await Log(_log);
+                        break; //don't think I need a break here lol oh well
+                    }
+                }
+            //};
+
+            //Function Commands
+            String[] funcCmds = new String[]
+            {
+                "!osu",
+                "!twitter"
+            };
+            
+            if(msg.StartsWith(funcCmds[0]))
+            {
+                String _user = msg.Substring(funcCmds[0].Length+1);
+                await message.Channel.SendMessageAsync("https://osu.ppy.sh/u/"+_user);
+                String _logMessage = "Issued !osu command for "+_user;
+                LogMessage _log = new LogMessage(LogSeverity.Info, "User", _logMessage);
+                await Log(_log);
+            }
+
+            if (msg.StartsWith(funcCmds[1]))
+            {
+                String _user = msg.Substring(funcCmds[1].Length+1);
+                await message.Channel.SendMessageAsync("https://twitter.com/" + _user);
+                String _logMessage = "Issued !twitter command for " + _user;
+                LogMessage _log = new LogMessage(LogSeverity.Info, "User", _logMessage);
+                await Log(_log);
+            }
+
+        }
+
+        
+    }
+}
---
+++ GigabyteConverter/GigabyteConverter/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GigabyteConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.labelGigabytes = new System.Windows.Forms.Label();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(466, 68);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Convert!";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            // 
+            // labelGigabytes
+            // 
+            this.labelGigabytes.AutoSize = true;
+            this.labelGigabytes.Location = new System.Drawing.Point(108, 393);
+            this.labelGigabytes.Name = "labelGigabytes";
+            this.labelGigabytes.Size = new System.Drawing.Size(69, 13);
+            this.labelGigabytes.TabIndex = 2;
+            this.labelGigabytes.Text = "Gigabytes: -1";
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(77, 71);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(100, 20);
+            this.textInput.TabIndex = 3;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "Bits",
+            "Bytes",
+            "Kilobytes",
+            "Megabytes",
+            "Gigabytes"});
+            this.comboBox.Location = new System.Drawing.Point(254, 70);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 21);
+            this.comboBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.labelGigabytes);
+            this.Controls.Add(this.buttonConvert);
+            this.Name = "Form1";
+            this.Text = "AnimNation\'s Gigabyte Converter!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.Label labelGigabytes;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
+
